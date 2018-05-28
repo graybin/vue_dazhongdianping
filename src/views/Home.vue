01fc3ae1cfbddd26dc91f4c5d5ec60031029d100
lin <1248075286@qq.com>
2018-05-29 00:35:25 +0800

    create icons branch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
     <home-header></home-header>
+    <div style="height:3.57rem;"></div>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import homeHeader from  '@/components/Header.vue'
+import homeIcons from '@/components/icons.vue'
 
 export default {
   name: 'home',
   components: {
-    homeHeader
+    homeHeader,
+    homeIcons
   }
 }
 </script>
