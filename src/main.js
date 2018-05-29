98926e43aded4452143a0a36633358c7c167b1f5
lin <1248075286@qq.com>
2018-05-30 01:34:31 +0800

    homeicons layout

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './registerServiceWorker'
 import './assets/scss/reset.css'
 import './assets/scss/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
